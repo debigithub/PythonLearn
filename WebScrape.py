d548ebb6b70a486f85385191c3874cc0d92bbd23
cloudera <mail2debi@gmail.com>
2017-07-04 00:21:32 +0100

    4-Jul-2017

--- WebScrape.py
+++ WebScrape.py
@@ -2,4 +2,6 @@ from urllib.request import Request,urlopen
 
 url=Request("http://www.bbc.co.uk",headers={'User-Agent': 'Mozilla/5.0'})
 f=urlopen(url)
-print(f.readlines())
+writer=open('datadump.txt','w')
+writer.write(str(f.readlines()))
+writer.close()
