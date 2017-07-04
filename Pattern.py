a022f92c9fdaad4d937e4278b5c82f1b59f4f1fa
cloudera <mail2debi@gmail.com>
2017-07-05 00:23:16 +0100

    4-Jul-2017

--- Pattern.py
+++ Pattern.py
@@ -1,10 +1,21 @@
 import re
-f=open('Baahubali.txt','r')
-final_list=list()
-for line in f.readlines():
-    a=re.findall('Prabhas',line)
-    final_list=final_list+a
-    print line
+from collections import Counter
+reader=open('datadump.txt','r')
+writer=open('datadump_cleansed.txt','w')
+Source='BBC.CO.UK'
+Today='2017-07-04'
+for x in reader.readlines():
+    c=Counter(re.findall('[a-zA-Z0-9]+', x))
+    for k,v in c.items():
+        #print('word %s repeated %s times' % (k,v))
+        if len(k) > 1:
+            print('%s,%s,%s,%s' % (Today,Source,k,v))
+            print('\n')
+ #   writer.write(x)
+  #  writer.write(' ')
 
-print len(final_list)
-f.close()
+
+
+
+
+
--- filetest.py
+++ filetest.py
@@ -1,4 +1,4 @@
 import re
 line='who misra abc mish12ra misdfsdfsdra'
 m=re.findall('mi[a-z]*[\d]*ra',line)
-print m
+print (m)
